719722689652092f1b88967d14447001cf98379d
Yusuf Soytürk <164682431+yusufsoyturk@users.noreply.github.com>
2025-08-10 22:02:11 +0000

    struct done

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -9,6 +9,32 @@
 #include <sys/time.h>   // gettimeofday
 #include <pthread.h>	// pthreads için 
 
+typedef struct	s_philo
+{
+	int				id;
+	pthread_t		thread;
+	size_t			last_meal; // ölüm zamanı için
+	int				meals_eaten; //opsiyonel bölüm için
+	pthread_mutex_t *r_fork;
+	pthread_mutex_t *l_fork;
+	struct s_prog	*prog;
+}				t_philo;
+
+typedef struct s_prog
+{
+	int				dead_flag;
+	pthread_mutex_t	write_lock;
+	pthread_mutex_t	meal_lock;
+	pthread_mutex_t	dead_lock;
+	pthread_mutex_t	*forks;
+	long			num_of_philos;
+	long			time_to_eat;
+	long			time_to_die;
+	long			time_to_sleep;
+	long            num_times_to_eat;
+	size_t			start_time;
+}			t_prog;
+
 long	ft_atol(char *str);
 int	arg_checker(char **av);
 int	arg_val_checker(char **av);
